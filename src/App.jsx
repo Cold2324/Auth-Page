dab41695dd873ae0a644d664455058e33fdfdec9
Gonzalo Cabada <cabadagonzalo17@gmail.com>
2022-12-31 21:39:47 -0500

    Routes Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import GreenContainer from './components/GreenContainer'
 import Login from './pages/Login'
 import Register from './pages/Register'
-import Successful from './pages/Successful'
 
 export default function App() {
   return (
     <main className="h-screen w-[100%] flex">
       <GreenContainer />
-      {/* <Login /> */}
-      {/* <Register /> */}
-      {/* <Successful /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/sign-up" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </main>
   )
 }
