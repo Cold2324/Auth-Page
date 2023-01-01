ac678ec9353070468cfa083bfc1759ca63e46578
Gonzalo Cabada <cabadagonzalo17@gmail.com>
2022-12-31 21:56:38 -0500

    firebase added

---
+++ src/firebase/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyATso8R2d2zl5CbR9e7fwZwVRRIAzyArEw',
+  authDomain: 'auth-app-b5443.firebaseapp.com',
+  projectId: 'auth-app-b5443',
+  storageBucket: 'auth-app-b5443.appspot.com',
+  messagingSenderId: '955955342802',
+  appId: '1:955955342802:web:ea862b5120d71b58786c0b',
+}
+
+const app = initializeApp(firebaseConfig)
